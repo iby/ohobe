ca9f18d9b98e40909ad05e7626e72812d9ef51ff
Ian Bytchek <ianbytchek@gmail.com>
2015-11-12 00:24:23 +0400

    BIGBANG!!!

---
+++ source/PointPixelAlign.jsx
@@ -0,0 +1,30 @@
+/**
+ * Aligns selected points in current selection to grid, by default 1×1 pixels.
+ *
+ * @param {number} [gridWidth]
+ * @param {number} [gridHeight]
+ */
+function main(gridWidth, gridHeight) {
+    gridWidth == null && (gridWidth = 1);
+    gridHeight == null && (gridHeight = 1);
+
+    var document = activeDocument;
+    var selection = document.selection;
+
+    // Go through all selected points.
+
+    for (var i = 0, n = selection.length; i < n; i++) {
+        if (selection[i].typename === "PathItem") {
+            var pathPoints = selection[i].selectedPathPoints;
+
+            for (var j = 0, m = pathPoints.length; j < m; j++) {
+                var point = pathPoints[j];
+                point.anchor = [Math.round(point.anchor[0] / gridWidth) * gridWidth, Math.round(point.anchor[1] / gridHeight) * gridHeight];
+                point.leftDirection = [Math.round(point.leftDirection[0] / gridWidth) * gridWidth, Math.round(point.leftDirection[1] / gridHeight) * gridHeight];
+                point.rightDirection = [Math.round(point.rightDirection[0] / gridWidth) * gridWidth, Math.round(point.rightDirection[1] / gridHeight) * gridHeight];
+            }
+        }
+    }
+}
+
+main();
